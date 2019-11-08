a81f91c41e5bf54343d36b846e1848c198c191c3
Harikrishnan KK <angularcity@gmail.com>
2019-11-08 07:56:47 -0500

    added redux persist

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import "./checkout-item.styles.scss";
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+import { connect } from "react-redux";
+import {
+  clearItemFromCart,
+  removeItem,
+  addItem
+} from "../../redux/cart/cart.actions";
+
+const CheckoutItem = ({ cartItem, clearItemFromCart, removeItem, addItem }) => (
   <div className="checkout-item">
     <div className="image-container">
-      <img src={imageUrl} alt="img" />
+      <img src={cartItem.imageUrl} alt="img" />
     </div>
 
-    <span className="name"> {name}</span>
-    <span className="quantity"> {quantity}</span>
-    <span className="price"> {price}</span>
-    <div className="remove-button"> &#10005;</div>
+    <span className="name"> {cartItem.name}</span>
+    <span className="quantity">
+      <div onClick={() => removeItem(cartItem)} className="arrow">
+        &#10094;
+      </div>
+      <span className="value"> {cartItem.quantity} </span>
+      <div onClick={() => addItem(cartItem)} className="arrow">
+        &#10095;
+      </div>
+    </span>
+
+    <span className="price"> {cartItem.price}</span>
+    <div onClick={() => clearItemFromCart(cartItem)} className="remove-button">
+      &#10005;
+    </div>
   </div>
 );
 
-export default CheckoutItem;
+export default connect(
+  null,
+  { clearItemFromCart, addItem, removeItem }
+)(CheckoutItem);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,18 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+
+const persistConfig = {
+  key: "root", // from what point we need to start storing?
+  storage,
+  whitelist: ["cart"] // only reducer we need to persist. User is handled by firebase ( add more reducer keys as app grows )
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+// modified version of root reducer
+export default persistReducer(persistConfig, rootReducer);
