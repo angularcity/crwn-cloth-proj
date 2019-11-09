413c56eca22f11901444898f7cfa9c88b9acdfe6
Harikrishnan KK <angularcity@gmail.com>
2019-11-09 09:10:40 -0500

    normalization done

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,16 +3,20 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
-  key: "root", // from what point we need to start storing?
+  key: "root",
   storage,
-  whitelist: ["cart"] // only reducer we need to persist. User is handled by firebase ( add more reducer keys as app grows )
+  whitelist: ["cart"]
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
-// modified version of root reducer
+
 export default persistReducer(persistConfig, rootReducer);
